998f857ca4331eb8a34a7a47238ce07a73b5765f
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-04 21:41:28 +0200

    Test 6 completed

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,7 @@
 export const filterNumbers = (array, largerThan) => {
-  return array
+  return array.filter(number => {
+    return number <= largerThan
+  })
 }
+
+//Here I attempted using splice as well, but filtering did a better job at completing the test
