33d1e3bc06378a6632da5505107ae36c2ae11697
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-04 20:49:42 +0200

    Test 5 completed

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -4,5 +4,18 @@ const months = [
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+  if (monthNumber === 0 || monthNumber > 12) {
+    return null
+  } else {
+    return months[monthNumber - 1]
+  }
 }
+//I also tried writing the function out like this:
+//if (monthNumber >= 12) { --AND also as (monthNumber <= 12)
+//   return null
+// } else {
+//   return months[monthNumber - 1]
+// }
+// }
+//Curiously enough, this did not work, instead I had to include the 0 in my if statement.
+//Guess that is because the last 'it' statement has a monthName of 0.
