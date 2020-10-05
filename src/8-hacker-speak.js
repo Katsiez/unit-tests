05c5979cdf7f938137a69bac54d204db4bb43d8d
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-05 15:43:08 +0200

    Looks like it worked for A and a to 4s, perhaps I need individual declarations, words.words.replace for all letters

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,6 +1,7 @@
 export const hackerSpeak = (words) => {
 
-  words = words.replace(/a|A/ig, '4', /e|E/ig, '3', /i|I/ig, '1', /o|O/ig, '0', /s|S/ig, '5')
+  words = words.replace(/a|A/ig, '4'),
+    (/e|E/ig, '3'), (/i|I/ig, '1'), (/o|O/ig, '0'), (/s|S/ig, '5')
   return words
 }
 
