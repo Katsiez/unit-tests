722478c4d7733719549e4f8ca47642c6fcef40a4
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-05 13:13:14 +0200

    Just wanted to commit this since it didn't work, so that I know what I've done in the past

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,19 @@
 export const hackerSpeak = (words) => {
-  return words
+  const dictionary = {
+    a: 4,
+    e: 3,
+    i: 1,
+    o: 0,
+    s: 5,
+    A: 4,
+    E: 3,
+    I: 1,
+    O: 0,
+    S: 5,
+  }
+  const hackerSpeak = (words) => {
+    return words.replace(/[aeios]/gi, (m) => dictionary[m])
+  }
+  document.write(hackerSpeak(''))
+  return (words)
 }
