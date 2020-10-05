5f43aa5f4746dfc05944bfa8d8fe55f8db25c039
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-05 15:38:07 +0200

    Okay, changed my approach entirely. I am going to try to separare all the words.replace statements by letter

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,20 +1,6 @@
+export const hackerSpeak = (words) => {
 
-//
-function hackerSpeak(words) {
-  return words.replace(/a/ig, '4')
-    // .replace(/A/ig, '4')
-    .replace(/e/ig, '3')
-    // .replace(/E/ig, '3')
-    .replace(/i/ig, '1')
-    // .replace(/I/ig, '1')
-    .replace(/o/ig, '0')
-    // .replace(/O/ig, '0')
-    .replace(/s/ig, '5')
-  // .replace(/S/ig, '5')
+  words = words.replace(/a|A/ig, '4', /e|E/ig, '3', /i|I/ig, '1', /o|O/ig, '0', /s|S/ig, '5')
+  return words
 }
-// hackerSpeak("javascript is cool")
-// hackerSpeak("programming is fun")
-// hackerSpeak("become a coder")
-hackerSpeak("JAVASCRIPT IS COOL")
-hackerSpeak("PROGRAMMING IS FUN")
-hackerSpeak("BECOME A CODER")
+
